4ef567dca2ef5cf90ac64eff1a9c5ec11be38391
Cristian F. Torres Collazo <cristian@inprende.com>
2026-02-17 12:20:35 -0400

    feat: enhance SEO and PWA support with updated meta tags, favicon, and manifest files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+// Unregister any stale service workers (e.g. from a previous PWA setup)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(sw => sw.unregister())
+  })
+}
+
 import './i18n'
 import './index.css'
 import App from './App.tsx'
